9420f6e62f454767a698e6f1d082735d7c876b91
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2021-10-29 09:40:26 +0700

    Update Perkalian Matriks.cpp

--- Perkalian Matriks.cpp
+++ Perkalian Matriks.cpp
@@ -1,13 +1,13 @@
-// Tugas: Penjumlahan Matriks
 #include <iostream>
 #include <conio.h>
+
 using namespace std;
 
 int main(){
 	int i, j, m, n, matriks1[10][10], matriks2[10][10], hasil[10][10];
   
-	cout<<" --PROGRAM PENJUMLAHAN MATRIKS--"<<endl;
-	cout<<"================================="<<endl;
+	cout<<" PROGRAM PENJUMLAHAN MATRIKS"<<endl;
+	cout<<"-----------------------------"<<endl;
 	cout<<" Jumlah baris matriks : "; cin>>m;
 	cout<<" Jumlah kolom matriks : "; cin>>n;
 	
